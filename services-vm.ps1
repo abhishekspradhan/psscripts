77a0b53a746078a16ad7d03f826902db4059c550
abhishekspradhan <69381187+abhishekspradhan@users.noreply.github.com>
2020-08-08 18:14:21 +0530

    Update services-vm.ps1

--- services-vm.ps1
+++ services-vm.ps1
@@ -3,11 +3,11 @@ $services = (Get-Service)|Where name -eq "wuauserv"
 $serviceName = $services.Name
 $servicestatus = $services.Status
 
-if ($servicestatus -eq "Stopped"){
+if ($servicestatus -eq "Running"){
 
-Write-host "Attempting to start the service" -ForegroundColor Yellow
+Write-host "Attempting to stop the service" -ForegroundColor Yellow
 
-Start-Service -Name $serviceName -Force 
+Stop-Service -Name $serviceName -Force 
 
 Write-Host "Mission successfull" -ForegroundColor Cyan
 
